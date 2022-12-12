df2a2c4a2f98cfcde40e18ce3c288797df81b5a6
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2022-12-12 23:56:54 +0530

    Add files via upload

---
+++ Introduction_to_JS.js
@@ -0,0 +1,3 @@
+console.log("Hi there! I'm learing JavaScript!");
+
+// JavaScript is a dynamically typed language, which means that we can change the datatype of any variable in the runtime.
---
+++ Variables_in_JavaScript.js
@@ -0,0 +1,25 @@
+// Variable are used to store values of different data types.
+
+var a = 4;  // var is used to declare a variable.
+console.log(a);
+
+// JavaScript is a dynamically typed language, which means that we can change the datatype of any variable in the runtime as we can see below.
+
+a = "Aur kese ho!";
+console.log(a);
+
+//  var 8a = 0;  Not allowed this will throw error, variable name cannot start with a digit(number).
+
+
+// JavaScript is case sensitive which means, UPPERCASE & lowercase are treated as different entity.
+
+// Each variable that is declared below is different as JavaScript is case sensitive.
+let HARRY = 48;   
+let harry = 28;
+let hArry = 9;
+let Harry = 10
+
+console.log(HARRY);
+console.log(harry);
+console.log(hArry);
+console.log(Harry);
