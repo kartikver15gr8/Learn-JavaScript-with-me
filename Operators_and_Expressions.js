9e234ff8e3f2f60fe44e4cdc8f6e6a385c1715ce
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2022-12-13 15:57:39 +0530

    Add files via upload

---
+++ Operators_and_Expressions.js
@@ -0,0 +1,24 @@
+console.log("Operators in JavaScript!");
+
+let alpha = 10;
+let beta = 87;
+let a = 9;
+let b = 2;
+
+console.log("alpha + beta", alpha+beta);
+console.log("alpha * beta", alpha*beta);
+console.log("alpha / beta", alpha/beta);
+console.log("alpha - beta", alpha-beta);
+
+console.log("The reminder of beta / alpha", beta % alpha);
+
+console.log("a raised to the power b", a**b);
+
+a++;
+console.log(a);
+b--;
+console.log(b);
+
+
+
+alpha += 10;  // Same as alpha = alpha + 10
