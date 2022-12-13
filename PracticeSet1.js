30033d99e4974d08a42e5f39ea283d8e82ea5732
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2022-12-13 14:36:59 +0530

    Add files via upload

---
+++ PracticeSet1.js
@@ -0,0 +1,45 @@
+// Q1 Create a variable of type string and try to add a number to it.
+
+const a = "Hello world";
+let b = 8;
+console.log(a+b);
+
+// Here in the program, we declared 'a' as a string and then added '8' into it. but remember here is taken as 'string' not 'integer', because we know that we can't add string to numbers.
+
+// Q2 use 'typeof' operator to find the datatype of the string in last question
+
+console.log(typeof(a));
+console.log(typeof(b));
+
+// Q3 Create a const object in JavaScript. Can you change it to hold a number later?
+
+ const item = {
+    Harry : "Nimbus 2000",
+    Potter :"Hogwartz",
+    Harmine : "Vingardium Lavioursa"
+ };
+ console.log(item);
+
+//  item = 8;      // We can't change item as it was declared as const.
+
+// Q4 Try to add a new key to the const object in problem 3, were you able to do so?
+
+item["Ron"] = "Weisley";        // We can add elements in an object despite of declaring it as const. 
+console.log(item);
+
+// Also we can change the value of keys declared inside the object 'item'
+
+item["Harry"] = "Potter";
+console.log(item);
+
+// Q5 Write a JavaScript program to create a word-meaning dictionary of 5 words
+
+const Dictionary = {
+    Realm : "Area",
+    JavaScript : "Awesome",
+    Kartikey : "Brilliant",
+    Tata : "Ratan JI",
+    Elon : undefined
+};
+
+console.log(Dictionary);
