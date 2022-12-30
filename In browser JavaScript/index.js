b0949383e4ad2a3cc2a9680a9bf0cd7744d7e0fb
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2022-12-30 15:41:09 +0530

    Add files via upload

---
+++ In browser JavaScript/index.js
@@ -0,0 +1,24 @@
+// // prompt("Enter your name:");
+// // alert("Hello");
+// let a = document.getElementsByClassName("para");
+
+// let c = 7;
+// let b = 3;
+// console.log(c+" "+b);
+// console.log(c+b);
+
+
+// alert("Tell us the security code!");
+// let a = prompt("C'mon now");
+// let write = confirm("Do you really want to write 'a' on the page.");
+// if (write) {
+//     document.write(a);
+// }
+// else{
+//     document.write("We've not written the secret code in the document.")
+// }
+
+// let color = prompt("Enter a color");
+// document.body.style.background = color;
+
+// console.log(document.body.firstChild);
