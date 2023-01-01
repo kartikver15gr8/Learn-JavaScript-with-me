64559e1b7e86393f57689759b957988eb17639fa
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2023-01-01 15:21:44 +0530

    Create setInterval_and_setTimeout.js

---
+++ setInterval_and_setTimeout.js
@@ -0,0 +1,15 @@
+const sum = (a, b) => {
+  console.log("The sum of a and b is : " + (a + b));
+  a + b;
+}
+
+// setInterval(sum, 5000, 34, 53);
+
+const msg = () => {
+  console.log("Hey there how's it all going!");
+}
+
+// setTimeout 
+
+setTimeout(msg, 5000);
+console.log(msg);
